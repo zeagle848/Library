7ade5888fd3faeae5ab2bf3416329e41d7f09fd0
William <williamcarpenterfrank@gmail.com>
2021-12-06 16:14:52 +0200

    Refactored project to make use of webpack

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,116 +1,116 @@
 describe('Testing the library', () => {
-    beforeEach(() => {
-        cy.visit('http://127.0.0.1:5500/index.html');
-    });
-    it('Can populate library with books after clicking populate library button', () => {
-        cy.get('#populate-library-button').click();
-        cy.get('#cards-container').children().should('have.length', 3);
-        cy.get('#delete-library-button').click();
-    });
-    it('Displays the delete book button when you hover over the card element', () => {
-        cy.get('#populate-library-button').click();
-        cy.get('.card-element').first().rightclick();
-        cy.get('.delete-book').first().should('be.visible');
-        cy.get('#delete-library-button').click();
-    });
-    it('Can delete books and deleted books stay deleted after refresh', () => {
-        cy.get('#populate-library-button').click();
-        cy.get('.card-element').first().trigger('mouseover');
-        cy.get('.delete-book').first().click();
-        cy.reload();
-        cy.get('#cards-container').children().should('have.length', 2);
-        cy.get('#delete-library-button').click();
-    });
-    it('Can close submit form when the user clicks on the cancel input button', () => {
-        cy.get('#add-book-button').click();
-        cy.get('#form-container').should('be.visible');
-        cy.get('#cancel-input').click();
-        cy.get('#form-container').should('not.be.visible');
-    });
-    it('Can close submit form when the user clicks on the background', () => {
-        cy.get('#add-book-button').click();
-        cy.get('#form-container').should('be.visible');
-        cy.get('#form-wrapper').click({ force: true });
-        cy.get('#form-container').should('not.be.visible');
-    });
-    it("Doesn't submit the form when none of the fields are filled out", () => {
-        cy.get('#add-book-button').click();
-        cy.get('#submit-book-button').click();
-        cy.get('#form-container').should('be.visible');
-    });
-    it("Doesn't submit the form when a feild is left out", () => {
-        cy.get('#add-book-button').click();
-        cy.get('#title-input').type('The Secret History');
-        cy.get('#author-input').type('Donna Tartt');
-        cy.get('#submit-book-button').click();
-        cy.get('#form-container').should('be.visible');
-    });
-    it("Doesn't submit the form when the pages input includes characters other than numbers", () => {
-        cy.get('#add-book-button').click();
-        cy.get('#title-input').type('The Secret History');
-        cy.get('#author-input').type('Donna Tartt');
-        cy.get('#pages-input').type('123a');
-        cy.get('#submit-book-button').click();
-        cy.get('#form-container').should('be.visible');
-    });
-    it('Submits and closes the form when all input feilds are filled out correctly', () => {
-        cy.get('#add-book-button').click();
-        cy.get('#title-input').type('The Secret History');
-        cy.get('#author-input').type('Donna Tartt');
-        cy.get('#pages-input').type('123');
-        cy.get('#submit-book-button').click();
-        cy.get('#form-container').should('not.be.visible');
+  beforeEach(() => {
+    cy.visit('http://localhost:8081/');
+  });
+  it('Can populate library with books after clicking populate library button', () => {
+    cy.get('#populate-library-button').click();
+    cy.get('#cards-container').children().should('have.length', 3);
+    cy.get('#delete-library-button').click();
+  });
+  it('Displays the delete book button when you hover over the card element', () => {
+    cy.get('#populate-library-button').click();
+    cy.get('.card-element').first().rightclick();
+    cy.get('.delete-book').first().should('be.visible');
+    cy.get('#delete-library-button').click();
+  });
+  it('Can delete books and deleted books stay deleted after refresh', () => {
+    cy.get('#populate-library-button').click();
+    cy.get('.card-element').first().trigger('mouseover');
+    cy.get('.delete-book').first().click();
+    cy.reload();
+    cy.get('#cards-container').children().should('have.length', 2);
+    cy.get('#delete-library-button').click();
+  });
+  it('Can close submit form when the user clicks on the cancel input button', () => {
+    cy.get('#add-book-button').click();
+    cy.get('#form-container').should('be.visible');
+    cy.get('#cancel-input').click();
+    cy.get('#form-container').should('not.be.visible');
+  });
+  it('Can close submit form when the user clicks on the background', () => {
+    cy.get('#add-book-button').click();
+    cy.get('#form-container').should('be.visible');
+    cy.get('#form-wrapper').click({ force: true });
+    cy.get('#form-container').should('not.be.visible');
+  });
+  it("Doesn't submit the form when none of the fields are filled out", () => {
+    cy.get('#add-book-button').click();
+    cy.get('#submit-book-button').click();
+    cy.get('#form-container').should('be.visible');
+  });
+  it("Doesn't submit the form when a feild is left out", () => {
+    cy.get('#add-book-button').click();
+    cy.get('#title-input').type('The Secret History');
+    cy.get('#author-input').type('Donna Tartt');
+    cy.get('#submit-book-button').click();
+    cy.get('#form-container').should('be.visible');
+  });
+  it("Doesn't submit the form when the pages input includes characters other than numbers", () => {
+    cy.get('#add-book-button').click();
+    cy.get('#title-input').type('The Secret History');
+    cy.get('#author-input').type('Donna Tartt');
+    cy.get('#pages-input').type('123a');
+    cy.get('#submit-book-button').click();
+    cy.get('#form-container').should('be.visible');
+  });
+  it('Submits and closes the form when all input feilds are filled out correctly', () => {
+    cy.get('#add-book-button').click();
+    cy.get('#title-input').type('The Secret History');
+    cy.get('#author-input').type('Donna Tartt');
+    cy.get('#pages-input').type('123');
+    cy.get('#submit-book-button').click();
+    cy.get('#form-container').should('not.be.visible');
 
-        cy.get('#delete-library-button').click();
-    });
-    it('Displays the card element with the same information that was filled out in the form', () => {
-        cy.get('#add-book-button').click();
-        cy.get('#title-input').type('The Secret History');
-        cy.get('#author-input').type('Donna Tartt');
-        cy.get('#pages-input').type('123');
-        cy.get('#submit-book-button').click();
+    cy.get('#delete-library-button').click();
+  });
+  it('Displays the card element with the same information that was filled out in the form', () => {
+    cy.get('#add-book-button').click();
+    cy.get('#title-input').type('The Secret History');
+    cy.get('#author-input').type('Donna Tartt');
+    cy.get('#pages-input').type('123');
+    cy.get('#submit-book-button').click();
 
-        cy.get('.card-title').should('have.text', 'The Secret History');
-        cy.get('.card-author').should('have.text', 'Donna Tartt');
+    cy.get('.card-title').should('have.text', 'The Secret History');
+    cy.get('.card-author').should('have.text', 'Donna Tartt');
 
-        cy.get('.card-pages').should('have.text', '123 pages');
-        cy.get('.card-checkbox').should('not.be.checked');
+    cy.get('.card-pages').should('have.text', '123 pages');
+    cy.get('.card-checkbox').should('not.be.checked');
 
-        cy.get('#delete-library-button').click();
-    });
-    it('Keeps a book marked read marked read', () => {
-        cy.get('#add-book-button').click();
-        cy.get('#title-input').type('The Secret History');
-        cy.get('#author-input').type('Donna Tartt');
-        cy.get('#pages-input').type('123');
-        cy.get('#submit-book-button').click();
+    cy.get('#delete-library-button').click();
+  });
+  it('Keeps a book marked read marked read', () => {
+    cy.get('#add-book-button').click();
+    cy.get('#title-input').type('The Secret History');
+    cy.get('#author-input').type('Donna Tartt');
+    cy.get('#pages-input').type('123');
+    cy.get('#submit-book-button').click();
 
-        cy.get('.card-checkbox').check();
-        cy.reload();
+    cy.get('.card-checkbox').check();
+    cy.reload();
 
-        cy.get('.card-checkbox').should('be.checked');
+    cy.get('.card-checkbox').should('be.checked');
 
-        cy.get('#delete-library-button').click();
-    });
-    it('Removes books based on ID', () => {
-        cy.get('#add-book-button').click();
-        cy.get('#title-input').type('The Secret History');
-        cy.get('#author-input').type('Donna Tartt');
-        cy.get('#pages-input').type('123');
-        cy.get('#submit-book-button').click();
+    cy.get('#delete-library-button').click();
+  });
+  it('Removes books based on ID', () => {
+    cy.get('#add-book-button').click();
+    cy.get('#title-input').type('The Secret History');
+    cy.get('#author-input').type('Donna Tartt');
+    cy.get('#pages-input').type('123');
+    cy.get('#submit-book-button').click();
 
-        cy.get('#add-book-button').click();
-        cy.get('#title-input').type('The Secret History');
-        cy.get('#author-input').type('Donna Tartt');
-        cy.get('#pages-input').type('123');
-        cy.get('#submit-book-button').click();
+    cy.get('#add-book-button').click();
+    cy.get('#title-input').type('The Secret History');
+    cy.get('#author-input').type('Donna Tartt');
+    cy.get('#pages-input').type('123');
+    cy.get('#submit-book-button').click();
 
-        cy.get('.card-element').first().trigger('mouseover');
-        cy.get('.delete-book').first().click();
+    cy.get('.card-element').first().trigger('mouseover');
+    cy.get('.delete-book').first().click();
 
-        cy.reload();
+    cy.reload();
 
-        cy.get('#cards-container').children().should('have.length', 1);
-        cy.get('#delete-library-button').click();
-    });
+    cy.get('#cards-container').children().should('have.length', 1);
+    cy.get('#delete-library-button').click();
+  });
 });
---
+++ src/index.js
@@ -0,0 +1,189 @@
+import "./style.css";
+import { closeForm, clearLibrary } from "./utils/functions.js";
+import { Library } from "./stores/library.js";
+
+const body = document.getElementsByClassName("body")[0];
+const formWrapper = document.getElementById("form-wrapper");
+
+const myLibrary = new Library();
+
+populateLibrary(myLibrary.getLibrary());
+
+function generateID() {
+  return Math.floor(Math.random() * 10000 + 1);
+}
+
+function openForm() {
+  formWrapper.classList.add("form-wrapper-visible");
+  body.classList.add("body-no-scroll");
+}
+
+function addEventForDelete(deleteButton, bookID) {
+  const removeSelectedBook = () => {
+    myLibrary.removeBook(bookID);
+    // deleteButton.removeEventListener('click', removeSelectedBook);
+    deleteButton.parentElement.remove();
+  };
+  deleteButton.addEventListener("click", removeSelectedBook);
+}
+
+function createCard(title, author, pages, isRead, bookID) {
+  const cardContainer = document.querySelector("#cards-container");
+  const fragment = document.createDocumentFragment();
+
+  const cardElement = document.createElement("div");
+  cardElement.setAttribute("class", "card-element");
+  cardElement.setAttribute("data-book-id", `${bookID}`);
+
+  const deleteBookButton = document.createElement("p");
+  deleteBookButton.setAttribute("class", "delete-book");
+  deleteBookButton.textContent = "X";
+  addEventForDelete(deleteBookButton, bookID);
+
+  cardElement.addEventListener("mouseover", () => {
+    deleteBookButton.classList.add("delete-book-visible");
+  });
+
+  cardElement.addEventListener("mouseout", () => {
+    deleteBookButton.classList.remove("delete-book-visible");
+  });
+
+  cardElement.append(deleteBookButton);
+
+  const cardHeader = document.createElement("div");
+  cardHeader.setAttribute("class", "card-header");
+  cardElement.append(cardHeader);
+
+  const cardTitle = document.createElement("span");
+  cardTitle.setAttribute("class", "card-content");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = `${title}`;
+  cardHeader.append(cardTitle);
+
+  const cardBy = document.createElement("span");
+  cardBy.setAttribute("class", "by-element");
+  cardBy.textContent = "by";
+  cardHeader.append(cardBy);
+
+  const cardAuthor = document.createElement("span");
+  cardAuthor.setAttribute("class", "card-content");
+  cardAuthor.classList.add("card-author");
+  cardAuthor.textContent = `${author}`;
+  cardHeader.append(cardAuthor);
+
+  const cardFooter = document.createElement("div");
+  cardFooter.setAttribute("class", "card-footer");
+  cardElement.append(cardFooter);
+
+  const cardPages = document.createElement("span");
+  cardPages.setAttribute("class", "card-content, card-pages");
+  cardPages.classList.add("card-pages");
+  cardPages.textContent = `${pages} pages`;
+  cardFooter.append(cardPages);
+
+  const cardReadBookSpan = document.createElement("span");
+  cardReadBookSpan.setAttribute("class", "card-read-book-span");
+  cardReadBookSpan.textContent = "Have you read this book?";
+  cardFooter.append(cardReadBookSpan);
+
+  const cardReadBookCheckbox = document.createElement("input");
+  cardReadBookCheckbox.setAttribute("type", "checkbox");
+  cardReadBookCheckbox.setAttribute("class", "card-read-book-checkbox");
+  cardReadBookCheckbox.classList.add("card-checkbox");
+  cardReadBookCheckbox.checked = isRead;
+  myLibrary.toggleIsRead(cardReadBookCheckbox, bookID);
+  cardReadBookSpan.append(cardReadBookCheckbox);
+
+  fragment.append(cardElement);
+  cardContainer.append(fragment);
+}
+
+function populateLibrary(library) {
+  clearLibrary();
+  library.forEach((book) => {
+    createCard(book.title, book.author, book.pages, book.isRead, book.id);
+  });
+}
+
+function saveBook() {
+  const titleElement = document.querySelector("#title-input");
+  const authorElement = document.querySelector("#author-input");
+  const pagesElement = document.querySelector("#pages-input");
+  const isReadElement = document.querySelector("#read-book-checkbox");
+
+  const title = titleElement.value.trim();
+  const author = authorElement.value.trim();
+  const pages = pagesElement.value.trim();
+  const isRead = isReadElement.checked;
+  const bookID = generateID();
+
+  if (
+    title === "" ||
+    author === "" ||
+    pages === "" ||
+    pagesElement.patternMismatch === true
+  ) {
+    return;
+  } else {
+    myLibrary.addBook(title, author, pages, isRead, bookID);
+    createCard(title, author, pages, isRead, bookID);
+    closeForm();
+  }
+}
+
+formWrapper.addEventListener("click", (event) => {
+  if (event.target === formWrapper) {
+    closeForm();
+  }
+});
+
+document.getElementById("cancel-input").addEventListener("click", closeForm);
+
+document.getElementById("add-book-button").addEventListener("click", openForm);
+
+document
+  .getElementById("refresh-library-button")
+  .addEventListener("click", () => {
+    populateLibrary(myLibrary.getLibrary());
+  });
+
+document
+  .getElementById("delete-library-button")
+  .addEventListener("click", () => {
+    myLibrary.deleteLibrary();
+    populateLibrary(myLibrary.getLibrary());
+  });
+
+document
+  .getElementById("populate-library-button")
+  .addEventListener("click", () => {
+    const testData = [
+      myLibrary.Book(
+        "The Secret History",
+        "Donna Tartt",
+        "551",
+        true,
+        generateID()
+      ),
+      myLibrary.Book(
+        "The Little Friend",
+        "Donna Tartt",
+        "643",
+        false,
+        generateID()
+      ),
+      myLibrary.Book(
+        "The Goldfinch",
+        "Donna Tartt",
+        "1006",
+        true,
+        generateID()
+      ),
+    ];
+    testData.forEach(({ title, author, pages, isRead, id }) => {
+      myLibrary.addBook(title, author, pages, isRead, id);
+      createCard(title, author, pages, isRead, id);
+    });
+  });
+
+window.saveBook = saveBook;
